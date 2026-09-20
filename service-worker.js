d059aed640554c12f66d53a256b2589b7467ba31
rolfbischoff <surfsau@gmx.de>
2026-09-20 10:59:04 +0200

    Implement service worker with caching and fetch logic
    Refactored service worker to implement caching and fetch handling.

--- service-worker.js
+++ service-worker.js
@@ -1,13 +1,70 @@
-<script>
-if ("serviceWorker" in navigator) {
-    window.addEventListener("load", () => {
-        navigator.serviceWorker.register("./service-worker.js")
-            .catch(error => {
-                console.error(
-                    "Service Worker konnte nicht registriert werden:",
-                    error
+"use strict";
+
+const CACHE_NAME = "mc-trainer-v1";
+
+const DATEIEN = [
+    "./",
+    "./index.html",
+    "./style.css",
+    "./app.js",
+    "./manifest.json",
+    "./fragenkatalog.xml",
+    "./icon-192.png",
+    "./icon-512.png"
+];
+
+self.addEventListener("install", event => {
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+            .then(cache => cache.addAll(DATEIEN))
+            .then(() => self.skipWaiting())
+    );
+});
+
+self.addEventListener("activate", event => {
+    event.waitUntil(
+        caches.keys()
+            .then(keys => {
+                return Promise.all(
+                    keys
+                        .filter(key => key !== CACHE_NAME)
+                        .map(key => caches.delete(key))
                 );
-            });
-    });
-}
-</script>
+            })
+            .then(() => self.clients.claim())
+    );
+});
+
+self.addEventListener("fetch", event => {
+    event.respondWith(
+        caches.match(event.request)
+            .then(cachedResponse => {
+
+                if (cachedResponse) {
+                    return cachedResponse;
+                }
+
+                return fetch(event.request)
+                    .then(response => {
+
+                        if (
+                            response &&
+                            response.status === 200 &&
+                            response.type === "basic"
+                        ) {
+                            const kopie = response.clone();
+
+                            caches.open(CACHE_NAME)
+                                .then(cache => {
+                                    cache.put(
+                                        event.request,
+                                        kopie
+                                    );
+                                });
+                        }
+
+                        return response;
+                    });
+            })
+    );
+});
