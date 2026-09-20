b984d1d20803cbe5d4198c871e2037c4c9778a4e
rolfbischoff <surfsau@gmx.de>
2026-09-20 12:05:41 +0200

    Refactor service worker for new cache and logging
    Updated service worker to use a new cache name and simplified event handling.

--- service-worker.js
+++ service-worker.js
@@ -1,70 +1,31 @@
 "use strict";
 
-const CACHE_NAME = "mc-trainer-v1";
-
-const DATEIEN = [
-    "./",
-    "./index.html",
-    "./style.css",
-    "./app.js",
-    "./manifest.json",
-    "./fragenkatalog.xml",
-    "./icon-192.png",
-    "./icon-512.png"
-];
+const CACHE_NAME = "mc-trainer-test-v1";
 
 self.addEventListener("install", event => {
-    event.waitUntil(
-        caches.open(CACHE_NAME)
-            .then(cache => cache.addAll(DATEIEN))
-            .then(() => self.skipWaiting())
-    );
+
+console.log("Service Worker: INSTALL");
+
+event.waitUntil(
+    self.skipWaiting()
+);
+
 });
 
 self.addEventListener("activate", event => {
-    event.waitUntil(
-        caches.keys()
-            .then(keys => {
-                return Promise.all(
-                    keys
-                        .filter(key => key !== CACHE_NAME)
-                        .map(key => caches.delete(key))
-                );
-            })
-            .then(() => self.clients.claim())
-    );
+
+console.log("Service Worker: ACTIVATE");
+
+event.waitUntil(
+    self.clients.claim()
+);
+
 });
 
 self.addEventListener("fetch", event => {
-    event.respondWith(
-        caches.match(event.request)
-            .then(cachedResponse => {
 
-                if (cachedResponse) {
-                    return cachedResponse;
-                }
+event.respondWith(
+    fetch(event.request)
+);
 
-                return fetch(event.request)
-                    .then(response => {
-
-                        if (
-                            response &&
-                            response.status === 200 &&
-                            response.type === "basic"
-                        ) {
-                            const kopie = response.clone();
-
-                            caches.open(CACHE_NAME)
-                                .then(cache => {
-                                    cache.put(
-                                        event.request,
-                                        kopie
-                                    );
-                                });
-                        }
-
-                        return response;
-                    });
-            })
-    );
 });
